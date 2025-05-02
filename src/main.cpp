6762f01c01013742a0d4f8ac70c18febd98f9c87
PickleGherkin <gknselcuk@gmail.com>
2025-05-02 19:12:23 +0200

    updated exit code when no arguments

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@ int main(int argc, char **argv)
     if(name == "" && number == -1)
     {
         app.exit(CLI::CallForHelp());
+        return 1;
     }
     oddOrEven(number);
     greeting(name);
